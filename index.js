6d098aa0d5606cbff7c6005c403b062db85a3618
Tobi <107647385+TurkiNAlghamdii@users.noreply.github.com>
2023-08-10 16:25:10 +0300

    Linked HTML elements to JS.

--- index.js
+++ index.js
@@ -1,64 +1,78 @@
+// - Problem: Storing the two number in variables
+// - Then continue with the rest
+
 let buttons = document.querySelectorAll("button");
-let buttonsCount = buttons.length
-for (var i = 0; i <= buttonsCount; i += 1) {
-    buttons[i].onclick = function(e) {
-        alert(this.id);
-    };
+let screen = document.getElementById("screen")
+
+
+
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    return "WTF?!";
+  }
+  return a / b;
 }
 
-function add(a,b){
-    
-    return a + b;
-}
-function sub(a,b){
-    return a - b;
-}
-function multiply(a,b){
-    return a * b;
-}
-function divide(a,b){
-    if(b ===0){
-        return "WTF?!";
-    }
-    return a / b;
+function toPercent(a) {
+  return a / 100;
 }
 
-function toPercent(a){
-    return a/100;
+function opposite(a) {
+  if (a > 0) {
+    return -a;
+  } else {
+    return a * -1;
+  }
 }
 
-function opposite(a){
-    if(a>0){
-        return -a;
+let num1, num2, opr;
+buttons.forEach(button => button.addEventListener("click", (e)=>{
+    if(e.target.id ==="+" || e.target.id ==="-" || e.target.id ==="*" || e.target.id ==="/"){
+        opr = e.target.id;
     }else{
-        return a*-1;
+        screen.innerText =e.target.innerText;
+        save(e.target.innerText);
+        
+        
+        
     }
+}));
+
+function save(a){
+    num1 = a;
     
 }
 
-let num1,num2,opr;
-num1 = 5
-num2 = 5
-opr = '+'
 
 let result;
 
-function operate(n1,n2,opr){
-    switch (opr) {
-        case "+":
-            result = add(n1,n2);
-            break;
-        case "-":
-            result = sub(n1,n2)
-        break;
-        case "*":
-            result = multiply(n1,n2)
-        break;
-        case "/":
-            result = divide(n1,n2)
-        break;
-        default:
-            break;
-    }
+function operate(n1, n2, opr) {
+  switch (opr) {
+    case "+":
+      result = add(n1, n2);
+      break;
+    case "-":
+      result = sub(n1, n2);
+      break;
+    case "*":
+      result = multiply(n1, n2);
+      break;
+    case "/":
+      result = divide(n1, n2);
+      break;
+    default:
+      break;
+  }
 }
-operate(num1,num2,opr);
+operate(num1, num2, opr);
+
+
